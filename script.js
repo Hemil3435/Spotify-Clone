e000fd0fc6b42d0fa8945030cf7d85ed36b56b4e
Hemil <marcosisgod0585@gmail.com>
2023-03-10 22:13:54 +0530

    Spotify Clone

---
+++ script.js
@@ -0,0 +1,25 @@
+let audioElement = new Audio('music.mp3');
+let myProgressBar = document.getElementById('myProgressBar');
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
+})
+
+
+audioElement.addEventListener('timeupdate', ()=>{ 
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
